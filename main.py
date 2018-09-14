f950a0c29cbdff3c8c634e66b0a516a5e978e7f7
Brad <bradleynielsen@gmail.com>
2018-09-14 10:07:49 +0300

    added table maker to main

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ from ImageClass import Image
 from pathlib import Path
 
 #Open DB connection
-conn = sqlite3.connect('images.db')
+conn = sqlite3.connect(':memory:')
 c = conn.cursor()
 print("Starting DB connection")
+
+print("Creating Images Table")
+
+c.execute("""CREATE TABLE images (
+    filename text,
+    hash text,
+    size integer
+    )""")
 
 #Set Path string
 p = Path(r'R:\Images\Wallpapers')
@@ -27,16 +35,19 @@ for file in p.iterdir():
     hash = hashMod.hasher(file)
     # print(hash)
 
+    #insert recort into table
     img_record = Image(filename, size, hash)
+    print("Inserting " + hash)
     c.execute("INSERT INTO images VALUES (:filename, :size, :hash)",{'filename': img_record.filename, 'size': img_record.size, 'hash': img_record.hash})
     conn.commit()
 
+    # Find duplicate VALUES
+    # c.execute("SELECT * FROM images WHERE hash='img_record.hash'")
 
 
-c.execute("SELECT * FROM images")
-print(c.fetchall())
+# print(c.fetchall())
 
 
-#close DB connection
-conn.close()
-print("DB connection closed")
+# #close DB connection
+# conn.close()
+# print("DB connection closed")
